5fdd6a487e0083f4f49d2b03108937f4c07eb0d0
Sid Sharma <pompeiifreckles@github.com>
2021-02-26 22:40:52 +0530

    some changes

---
+++ netflix_anime/src/Components/Layout.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Container, Header, Content, Footer, Sidebar } from 'rsuite';
+
+function Layout() {
+    return (
+        <React.Fragment>
+            
+        </React.Fragment>
+    )
+}
+
+export default Layout
